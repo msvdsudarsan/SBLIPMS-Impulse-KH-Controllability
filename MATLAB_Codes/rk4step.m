fd215eea1413d7503bdfcc21de1e29a70d9c2be1
SRI VENKATA DURGA SUDARSAN MADHYANNAPU <msvdsudarsan@gmail.com>
2026-05-01 13:09:45 +0530

    Update rk4step.m

--- MATLAB_Codes/rk4step.m
+++ MATLAB_Codes/rk4step.m
@@ -3,7 +3,7 @@ function Phi_out = rk4step(E, Afun, Phi_in, t0, dt)
     Ep = pinv(E);   % ✅ SAFE for singular E
 
     f  = @(t, P) Ep * (Afun(t) * P);
-
+ 
     k1 = f(t0,        Phi_in);
     k2 = f(t0+dt/2,   Phi_in + (dt/2)*k1);
     k3 = f(t0+dt/2,   Phi_in + (dt/2)*k2);
